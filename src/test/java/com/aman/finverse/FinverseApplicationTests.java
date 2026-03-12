f879f0000ce51cf68bb8d4e3af0c23318e5a5023
Aman Painuly <aman.painuly@gmail.com>
2026-03-12 07:06:20 +0530

    spring boot appl created

---
+++ src/test/java/com/aman/finverse/FinverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aman.finverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
